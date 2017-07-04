ff97d8dfcb94cce813505af9c78143416c91628a
Michael Skrzypiec <skrzym@gmail.com>
2017-07-04 15:08:23 -0700

    adding mov renaming command line tool

---
+++ mov_rename_by_datetime.py
@@ -0,0 +1,117 @@
+import os
+import re
+import argparse
+
+# Code Source: https://stackoverflow.com/questions/21355316/getting-metadata-for-mov-video
+
+import datetime
+import struct
+
+parser = argparse.ArgumentParser(description="EXPERIMENTAL! WARNING THIS WILL CHANGE ALL FOUND .MOV FILENAMES! EXPERIMENTAL! This will rename all .MOV files in a directory to follow the following capture time format 'IMG_YYYYMMDD_HHMMSS.MOV'. If a conflict arises the script will increment the file name by 1 as such 'IMG_YYYYMMDD_HHMMSS_1.MOV'.")
+parser.add_argument('--path', help="file directory to look for MOV files to modify")
+parser.add_argument("-v", "--verbose", action='store_true',
+                    help="when set, output shows old and new file names for each modified file")
+args = parser.parse_args()
+
+
+def get_mov_datetime(filename):
+    ATOM_HEADER_SIZE = 8
+    # difference between Unix epoch and QuickTime epoch, in seconds
+    EPOCH_ADJUSTER = 2082844800
+
+    #if len(sys.argv) < 2:
+    #    print "USAGE: mov-length.py <file.mov>"
+    #    sys.exit(1)
+
+    # open file and search for moov item
+    f = open(filename, "rb")
+    while 1:
+        atom_header = f.read(ATOM_HEADER_SIZE)
+        if atom_header[4:8] == 'moov':
+            break
+        else:
+            atom_size = struct.unpack(">I", atom_header[0:4])[0]
+            f.seek(atom_size - 8, 1)
+
+    # found 'moov', look for 'mvhd' and timestamps
+    atom_header = f.read(ATOM_HEADER_SIZE)
+    if atom_header[4:8] == 'cmov':
+        print "moov atom is compressed"
+        return None
+    elif atom_header[4:8] != 'mvhd':
+        print "expected to find 'mvhd' header"
+        return None
+    else:
+        f.seek(4, 1)
+        creation_date = struct.unpack(">I", f.read(4))[0]
+        #modification_date = struct.unpack(">I", f.read(4))[0]
+        #print "creation date:",
+        #print datetime.datetime.utcfromtimestamp(creation_date - EPOCH_ADJUSTER)
+        #print "modification date:",
+        #print datetime.datetime.utcfromtimestamp(modification_date - EPOCH_ADJUSTER)
+        return datetime.datetime.utcfromtimestamp(creation_date - EPOCH_ADJUSTER)
+
+
+def list_dir_mov(basedir=os.path.curdir):
+    mov_files = []
+    other_files = []
+    sub_dirs = []
+    for item in os.listdir(basedir):
+        abs_path = os.path.join(basedir,item)
+        if os.path.isfile(abs_path):
+            if re.search('(\.MOV)',item):
+                mov_files.append(abs_path)
+            else:
+                other_files.append(abs_path)
+        else:
+            sub_dirs.append(abs_path)
+    return (mov_files,other_files,sub_dirs)
+
+
+def modify_mov_filename(directory=os.curdir):
+    movs,others,subdirs = list_dir_mov(directory)
+    for mov in movs:
+            oldmov = mov[:-4]+'_old'+mov[-4:]
+            os.rename(mov,oldmov)
+    movs,others,subdirs = list_dir_mov(directory)
+    
+    print '=MOV-FILENAME-MODIFIER'+ '='*17
+    print 'Ignored files:', len(others)
+    print 'Ignored sub directories:', len(subdirs)
+    print 'MOVs files to be edited:', len(movs)
+    print '='*40
+    if len(movs) == 0:
+        print 'There are no "*.MOV" files in supplied directory!'
+        print '='*40
+    else:
+        print 'Editing MOV Filenames...'
+        
+        for mov in movs:
+            
+            # fdt = datetime.datetime.strptime(piexif.load(mov)['0th'][306],"%Y:%m:%d %H:%M:%S")
+            fdt = get_mov_datetime(mov)
+
+            matches = [
+                item 
+                for item in os.listdir(directory) 
+                if re.search('MOV_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'(_?[0-9]*)\.[mM][oO][vV]', item)
+            ]
+            if len(matches) > 0:
+                if os.path.basename(mov) != 'MOV_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'.MOV':
+                    new_filename = 'MOV_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'_'+str(len(matches))+'.MOV'
+            else:
+                new_filename = 'MOV_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'.MOV'
+                
+            try:
+                os.rename(mov,directory+'\\'+new_filename)
+            except WindowsError, arg:
+                print(os.path.basename(mov),new_filename)
+                raise WindowsError, arg
+            if args.verbose:
+                print ' '*2 + os.path.basename(mov)[:-8] + ' --> ' + new_filename[:-4]
+        print '='*40
+        print 'Editing Complete!', str(len(movs)), 'files modified.'
+        print '='*40
+
+
+modify_mov_filename(directory=args.path)
---
+++ rename_by_datetime.py
@@ -0,0 +1,77 @@
+import os
+import datetime as dt
+import re
+import piexif
+import argparse
+from PIL.ExifTags import TAGS
+
+
+parser = argparse.ArgumentParser(description="EXPERIMENTAL! WARNING THIS WILL CHANGE ALL FOUND .JPG FILENAMES! EXPERIMENTAL! This will rename all JPG files in a directory to follow the following capture time format 'IMG_YYYYMMDD_HHMMSS.JPG'. If a conflict arises the script will increment the file name by 1 as such 'IMG_YYYYMMDD_HHMMSS_1.JPG'.")
+parser.add_argument('--path', help="file directory to look for JPG files to modify")
+parser.add_argument("-v", "--verbose", action='store_true',
+                    help="when set, output shows old and new file names for each modified file")
+args = parser.parse_args()
+
+
+def list_dir_jpeg(basedir=os.path.curdir):
+    jpeg_files = []
+    other_files = []
+    sub_dirs = []
+    for item in os.listdir(basedir):
+        abs_path = os.path.join(basedir,item)
+        if os.path.isfile(abs_path):
+            if re.search('(\.JPG)',item):
+                jpeg_files.append(abs_path)
+            else:
+                other_files.append(abs_path)
+        else:
+            sub_dirs.append(abs_path)
+    return (jpeg_files,other_files,sub_dirs)
+
+
+def modify_jpeg_filename(directory=os.curdir):
+    jpegs,others,subdirs = list_dir_jpeg(directory)
+    for pic in jpegs:
+            oldpic = pic[:-4]+'_old'+pic[-4:]
+            os.rename(pic,oldpic)
+    jpegs,others,subdirs = list_dir_jpeg(directory)
+    
+    print '=JPEG-FILENAME-MODIFIER'+ '='*17
+    print 'Ignored files:', len(others)
+    print 'Ignored sub directories:', len(subdirs)
+    print 'JPEG files to be edited:', len(jpegs)
+    print '='*40
+    if len(jpegs) == 0:
+        print 'There are no "*.JPG" files in supplied directory!'
+        print '='*40
+    else:
+        print 'Editing JPEG Filenames...'
+        
+        for pic in jpegs:
+            
+            fdt = dt.datetime.strptime(piexif.load(pic)['0th'][306],"%Y:%m:%d %H:%M:%S")
+            
+            matches = [
+                item 
+                for item in os.listdir(directory) 
+                if re.search('IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'(_?[0-9]*)\.[jJ][pP][gG]',item)
+            ]
+            if len(matches) > 0:
+                if os.path.basename(pic) != 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'.JPG':
+                    new_filename = 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'_'+str(len(matches))+'.JPG'
+            else:
+                new_filename = 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'.JPG'
+                
+            try:
+                os.rename(pic,directory+'\\'+new_filename)
+            except WindowsError, arg:
+                print(os.path.basename(pic),new_filename)
+                raise WindowsError, arg
+            if args.verbose:
+                print ' '*2 + os.path.basename(pic)[:-8] + ' --> ' + new_filename[:-4]
+        print '='*40
+        print 'Editing Complete!', str(len(jpegs)), 'files modified.'
+        print '='*40
+
+
+modify_jpeg_filename(directory=args.path)
