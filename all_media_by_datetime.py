97aa3fa63390a087c086949e410c3c953689108b
Michael Skrzypiec <skrzym@gmail.com>
2018-01-14 15:15:22 -0800

    GUI Implementation v0.1

--- all_media_by_datetime.py
+++ all_media_by_datetime.py
@@ -19,10 +19,25 @@ def is_live(file_list, filename):
     # If the list contains .JPG and .MOV then return True otherwise false
 
     file_extensions = [f.lower()[f.find('.'):] for f in file_list if f[0:f.find('.')] == filename[0:filename.find('.')]]
-    return ('.jpg' in file_extensions) & ('.mov' in file_extensions)
+    return ('.jpg' in file_extensions) & ('.mov' in file_extensions) & (filename.lower()[filename.find('.'):] in ['.mov','.jpg'])
+
+
+def is_heic_live(file_list, filename):
+    """
+    Checks file_list for filename containing .jpg and .mov files
+    :param file_list: list of all base file names including their extension
+    :param filename: string of filename to check for
+    :return: bool
+    """
+    # generate list of all filename extensions that have the provided filename.
+    # If the list contains .HEIC and .MOV  and NO .JPG then return True otherwise false
+
+    file_extensions = [f.lower()[f.find('.'):] for f in file_list if f[0:f.find('.')] == filename[0:filename.find('.')]]
+    return ('.heic' in file_extensions) & ('.mov' in file_extensions) & ('.jpg' not in file_extensions)
 
 
 def list_dir_files(basedir=os.path.curdir):
+
     live_files = []
     jpeg_files = []
     mov_files = []
@@ -36,11 +51,13 @@ def list_dir_files(basedir=os.path.curdir):
         abs_path = os.path.join(basedir,item)
         if os.path.isfile(abs_path):
             if is_live(only_files, item):
-                print item, 'is live file' #temp
                 live_files.append(abs_path)
-            if re.search('(\.JPG)',item):
+            elif is_heic_live(only_files, item):
+                # print item, 'is heic based live photo! BAD!'
+                other_files.append(abs_path)
+            elif re.search('(\.JPG)', item, re.IGNORECASE):
                 jpeg_files.append(abs_path)
-            elif re.search('(\.MOV)',item):
+            elif re.search('(\.MOV)', item, re.IGNORECASE):
                 mov_files.append(abs_path)
             else:
                 other_files.append(abs_path)
@@ -54,13 +71,17 @@ def logger(msg):
 
 
 def directory_status(directory=os.curdir, log=logger):
-    jpegs, others, subdirs = list_dir_files(directory)
+    lives, jpegs, movs, others, subdirs = list_dir_files(directory)
     log('=MEDIA-FILENAME-MODIFIER-PROGRAM' + '=' * 17)
     log('Ignored files: ' + str(len(others)))
-    log('Ignored sub directories: ' + str(len(subdirs)))
-    log('Files to be edited: ' + str(len(jpegs)))
+    log('Ignored folders: ' + str(len(subdirs)))
     log('=' * 40)
-    if len(jpegs) == 0:
+    log('Files to be edited: ' + str(len(jpegs) + len(lives) + len(movs)))
+    log('Live Photos: ' + str(len(lives)))
+    log('Regular Photos: ' + str(len(jpegs)))
+    log('Videos: ' + str(len(movs)))
+    log('=' * 40)
+    if (len(jpegs) + len(lives) + len(movs)) == 0:
         log('There are no valid media files in supplied directory!')
         log('=' * 40)
     else:
@@ -69,20 +90,117 @@ def directory_status(directory=os.curdir, log=logger):
         log('=' * 40)
 
 
-def modify_jpeg_filename(directory=os.curdir, log=logger, verbose=None):
-    jpegs, others, subdirs = list_dir_files(directory)
+def get_mov_datetime(filename):
+    ATOM_HEADER_SIZE = 8
+    # difference between Unix epoch and QuickTime epoch, in seconds
+    EPOCH_ADJUSTER = 2082844800
+
+    #if len(sys.argv) < 2:
+    #    print "USAGE: mov-length.py <file.mov>"
+    #    sys.exit(1)
+
+    # open file and search for moov item
+    f = open(filename, "rb")
+    while 1:
+        atom_header = f.read(ATOM_HEADER_SIZE)
+        if atom_header[4:8] == 'moov':
+            break
+        else:
+            atom_size = struct.unpack(">I", atom_header[0:4])[0]
+            f.seek(atom_size - 8, 1)
+
+    # found 'moov', look for 'mvhd' and timestamps
+    atom_header = f.read(ATOM_HEADER_SIZE)
+    if atom_header[4:8] == 'cmov':
+        print "moov atom is compressed"
+        return None
+    elif atom_header[4:8] != 'mvhd':
+        print "expected to find 'mvhd' header"
+        return None
+    else:
+        f.seek(4, 1)
+        creation_date = struct.unpack(">I", f.read(4))[0]
+        #modification_date = struct.unpack(">I", f.read(4))[0]
+        #print "creation date:",
+        #print datetime.datetime.utcfromtimestamp(creation_date - EPOCH_ADJUSTER)
+        #print "modification date:",
+        #print datetime.datetime.utcfromtimestamp(modification_date - EPOCH_ADJUSTER)
+        return dt.datetime.utcfromtimestamp(creation_date - EPOCH_ADJUSTER)# + dt.timedelta(days=args.days,hours=args.hours,minutes=args.minutes,seconds=args.seconds)
+
+
+def modify_media_filenames(directory=os.curdir, log=logger, verbose=None):
+    lives, jpegs, movs, others, subdirs = list_dir_files(directory)
+    for live in lives:
+        oldlive = live[:-4]+'_old'+live[-4:]
+        os.rename(live, oldlive)
+    for mov in movs:
+        oldmov = mov[:-4]+'_old'+mov[-4:]
+        os.rename(mov, oldmov)
     for pic in jpegs:
-            oldpic = pic[:-4]+'_old'+pic[-4:]
-            os.rename(pic, oldpic)
-    jpegs, others, subdirs = list_dir_files(directory)
+        oldpic = pic[:-4]+'_old'+pic[-4:]
+        os.rename(pic, oldpic)
+    lives, jpegs, movs, others, subdirs = list_dir_files(directory)
+
+    if len(lives) != 0:
+        log('Editing Live Photo Filenames...')
+
+        live_files = lives
+        live_pairs = []
+        while len(live_files) > 0:
+            cur_file = live_files.pop()
+            for index, element in enumerate(live_files):
+                if os.path.basename(cur_file).lower()[:-4] == os.path.basename(element).lower()[:-4]:
+                    if cur_file.lower()[-4:] == '.jpg':
+                        live_pairs.append((cur_file, live_files.pop(index)))
+                    else:
+                        live_pairs.append((live_files.pop(index), cur_file))
+
+        for pic, movie in live_pairs:
+            try:
+                fdt = dt.datetime.strptime(piexif.load(pic)['0th'][306], "%Y:%m:%d %H:%M:%S")
+            except KeyError:
+                log('Key Error! ' + os.path.basename(pic) + ' is being skipped!')
+                continue
+            matches = [
+                item
+                for item in os.listdir(directory)
+                if re.search('IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'(_?[0-9]*)\.[jJ][pP][gG]', item)
+            ]
+            if len(matches) > 0:
+                if os.path.basename(pic) != 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'.JPG':
+                    new_filename = 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'_'+str(len(matches))+'.JPG'
+                    new_videoname = 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'_'+str(len(matches))+'.MOV'
+            else:
+                new_filename = 'IMG_'+'{:%Y%m%d_%H%M%S}'.format(fdt)+'.JPG'
+                new_videoname = 'IMG_' + '{:%Y%m%d_%H%M%S}'.format(fdt) + '.MOV'
+
+            try:
+                os.rename(pic, directory + '\\' + new_filename)
+            except OSError, arg:
+                log(os.path.basename(pic) + new_filename)
+                raise OSError, arg
+
+            try:
+                os.rename(movie, directory + '\\' + new_videoname)
+            except OSError, arg:
+                log(os.path.basename(movie) + new_videoname)
+                raise OSError, arg
+
+            if verbose:
+                log(' '*2 + os.path.basename(pic)[:-8] + ' --> ' + new_filename[:-4])
+                log(' ' * 2 + os.path.basename(movie)[:-8] + ' --> ' + new_videoname[:-4])
+        log('Live Photos Editing Complete! ' + str(len(lives)) + ' files modified.')
+        log('=' * 40)
 
     if len(jpegs) != 0:
         log('Editing JPEG Filenames...')
 
         for pic in jpegs:
-
-            fdt = dt.datetime.strptime(piexif.load(pic)['0th'][306],"%Y:%m:%d %H:%M:%S")
-
+            try:
+                fdt = dt.datetime.strptime(piexif.load(pic)['0th'][306], "%Y:%m:%d %H:%M:%S")
+            except KeyError:
+                log('Key Error! ' + os.path.basename(pic) + ' is being skipped!')
+                continue
             matches = [
                 item
                 for item in os.listdir(directory)
@@ -97,13 +215,43 @@ def modify_jpeg_filename(directory=os.curdir, log=logger, verbose=None):
             try:
                 os.rename(pic,directory+'\\'+new_filename)
             except OSError, arg:
-                log(os.path.basename(pic),new_filename)
+                log(os.path.basename(pic) + new_filename)
                 raise OSError, arg
             if verbose:
                 log(' '*2 + os.path.basename(pic)[:-8] + ' --> ' + new_filename[:-4])
+        log('JPG Editing Complete! ' + str(len(jpegs)) + ' files modified.')
         log('='*40)
-        log('Editing Complete! ' + str(len(jpegs)) + ' files modified.')
-        log('='*40)
+
+    if len(movs) > 0:
+        log('Editing MOV Filenames...')
+
+        for mov in movs:
+
+            # fdt = datetime.datetime.strptime(piexif.load(mov)['0th'][306],"%Y:%m:%d %H:%M:%S")
+            fdt = get_mov_datetime(mov)
+
+            matches = [
+                item
+                for item in os.listdir(directory)
+                if re.search('MOV_' + '{:%Y%m%d_%H%M%S}'.format(fdt) + '(_?[0-9]*)\.[mM][oO][vV]', item)
+            ]
+            if len(matches) > 0:
+                if os.path.basename(mov) != 'MOV_' + '{:%Y%m%d_%H%M%S}'.format(fdt) + '.MOV':
+                    new_filename = 'MOV_' + '{:%Y%m%d_%H%M%S}'.format(fdt) + '_' + str(len(matches)) + '.MOV'
+            else:
+                new_filename = 'MOV_' + '{:%Y%m%d_%H%M%S}'.format(fdt) + '.MOV'
+
+            try:
+                os.rename(mov, directory + '\\' + new_filename)
+            except OSError, arg:
+                log(os.path.basename(mov) + new_filename)
+                raise OSError, arg
+            if verbose:
+                log(' ' * 2 + os.path.basename(mov)[:-8] + ' --> ' + new_filename[:-4])
+        log('MOV Editing Complete! ' + str(len(movs)) + ' files modified.')
+        log('=' * 40)
+    log('Conversions completed.')
+
 
 
 if __name__ == '__main__':
@@ -112,4 +260,4 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--verbose", action='store_true',
                         help="when set, output shows old and new file names for each modified file")
     args = parser.parse_args()
-    modify_jpeg_filename(directory=args.path, verbose=args.verbose)
+    modify_media_filenames(directory=args.path, verbose=args.verbose)
--- metadata-editor-gui.py
+++ metadata-editor-gui.py
@@ -1,4 +1,5 @@
 import Tkinter as tk
+import ttk
 import tkFileDialog
 import rename_by_datetime as rbd
 import all_media_by_datetime as ambd
@@ -10,8 +11,9 @@ class MetadataEditorGui:
 
     def __init__(self, master):
         self.master = master
+        self.IDLE = 'Waiting for user input...'
         #self.master.geometry("400x500")
-        self.master.title("Hello World")
+        self.master.title("Media Filename Modification Program")
         # self.master.resizable(False, False)
         # self.master.tk_setPalette(background='#e6e6e6')
 
@@ -26,23 +28,23 @@ class MetadataEditorGui:
         menu_bar = tk.Menu(master)
 
         # create a pull-down menu, and add it to the menu bar
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Open", command=self.click_ok)
-        file_menu.add_command(label="Save", command=self.click_ok)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.click_exit)
-        menu_bar.add_cascade(label="File", menu=file_menu)
+        menu_file = tk.Menu(menu_bar, tearoff=0)
+        menu_file.add_command(label="Open", command=self.click_ok)
+        menu_file.add_command(label="Save", command=self.click_ok)
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit", command=self.click_exit)
+        menu_bar.add_cascade(label="File", menu=menu_file)
 
         # create more pull-down menus
-        edit_menu = tk.Menu(menu_bar, tearoff=0)
-        edit_menu.add_command(label="Cut", command=self.click_ok)
-        edit_menu.add_command(label="Copy", command=self.click_ok)
-        edit_menu.add_command(label="Paste", command=self.click_ok)
-        menu_bar.add_cascade(label="Edit", menu=edit_menu)
+        menu_edit = tk.Menu(menu_bar, tearoff=0)
+        menu_edit.add_command(label="Cut", command=self.click_ok)
+        menu_edit.add_command(label="Copy", command=self.click_ok)
+        menu_edit.add_command(label="Paste", command=self.click_ok)
+        menu_bar.add_cascade(label="Edit", menu=menu_edit)
 
-        help_menu = tk.Menu(menu_bar, tearoff=0)
-        help_menu.add_command(label="About", command=self.click_ok)
-        menu_bar.add_cascade(label="Help", menu=help_menu)
+        menu_help = tk.Menu(menu_bar, tearoff=0)
+        menu_help.add_command(label="About", command=self.click_ok)
+        menu_bar.add_cascade(label="Help", menu=menu_help)
 
         # display the menu
         self.master.config(menu=menu_bar)
@@ -50,47 +52,121 @@ class MetadataEditorGui:
         # ******************** MAIN LAYOUT ********************
         # Main App Frame
         self.frame = tk.Frame(self.master)
-        self.frame.pack(side=tk.TOP, fill=tk.BOTH, padx=10, pady=10)
+        self.frame.pack(expand=True, side=tk.TOP, fill=tk.BOTH, padx=20, pady=20)
+        self.frame.rowconfigure(0, weight=1)
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.columnconfigure(1, weight=1)
         # self.frame['background'] = '#FFFFFF'
 
-        # Source Photos --------------------
+        # LEFT SIDE
+        self.frame_list_1 = tk.Frame(self.frame)
+        self.notebook_1 = ttk.Notebook(self.frame_list_1)
 
-        self.list_1_frame = tk.Frame(self.frame)
-        self.label_1 = tk.Label(self.list_1_frame, text='No Folder Selected')
-        self.label_1.pack(side=tk.TOP, fill=tk.X)
-        self.scrollbar_1 = tk.Scrollbar(self.list_1_frame)
-        self.scrollbar_1.pack(side=tk.RIGHT, fill=tk.Y)
-        self.listbox_1 = tk.Listbox(self.list_1_frame, width=40, height=25)
-        self.listbox_1.pack()
-        self.listbox_1.config(yscrollcommand=self.scrollbar_1.set)
-        self.scrollbar_1.config(command=self.listbox_1.yview)
-        self.list_1_frame.grid(row=1, column=0)
+        # Notebook Tab 1 - Live Photos
+        self.frame_tab_live = tk.Frame(self.notebook_1)
+        self.scrollbar_live = tk.Scrollbar(self.frame_tab_live)
+        self.scrollbar_live.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
+        self.listbox_live = tk.Listbox(self.frame_tab_live, width=50, height=20)
+        self.listbox_live.pack(expand=True, fill=tk.BOTH)
+        self.listbox_live.config(yscrollcommand=self.scrollbar_live.set)
+        self.scrollbar_live.config(command=self.listbox_live.yview)
 
-        self.list_2_frame = tk.Frame(self.frame)
-        self.label_2 = tk.Label(self.list_2_frame, text='Progress Details')
-        self.label_2.pack(side=tk.TOP, fill=tk.X)
-        self.button_run = tk.Button(self.list_2_frame, text='Run', command=self.run, width=20)
-        self.button_run.pack(side=tk.BOTTOM)
+        # Notebook Tab 2 - Regular JPEGS
+        self.frame_tab_photos = tk.Frame(self.notebook_1)
+        self.scrollbar_photos = tk.Scrollbar(self.frame_tab_photos)
+        self.scrollbar_photos.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
+        self.listbox_photos = tk.Listbox(self.frame_tab_photos, width=50, height=20)
+        self.listbox_photos.pack(expand=True, fill=tk.BOTH)
+        self.listbox_photos.config(yscrollcommand=self.scrollbar_photos.set)
+        self.scrollbar_photos.config(command=self.listbox_photos.yview)
+
+        # Notebook Tab 3 - Movies
+        self.frame_tab_videos = tk.Frame(self.notebook_1)
+        self.scrollbar_videos = tk.Scrollbar(self.frame_tab_videos)
+        self.scrollbar_videos.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
+        self.listbox_videos = tk.Listbox(self.frame_tab_videos, width=50, height=20)
+        self.listbox_videos.pack(expand=True, fill=tk.BOTH)
+        self.listbox_videos.config(yscrollcommand=self.scrollbar_videos.set)
+        self.scrollbar_videos.config(command=self.listbox_videos.yview)
+
+        # Notebook Tab 4 - Other Files
+        self.frame_tab_other = tk.Frame(self.notebook_1)
+        self.scrollbar_other = tk.Scrollbar(self.frame_tab_other)
+        self.scrollbar_other.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
+        self.listbox_other = tk.Listbox(self.frame_tab_other, width=50, height=20)
+        self.listbox_other.pack(expand=True, fill=tk.BOTH)
+        self.listbox_other.config(yscrollcommand=self.scrollbar_other.set)
+        self.scrollbar_other.config(command=self.listbox_other.yview)
+
+        # Notebook Tab 5 - Sub Folders
+        self.frame_tab_folders = tk.Frame(self.notebook_1)
+        self.scrollbar_folders = tk.Scrollbar(self.frame_tab_folders)
+        self.scrollbar_folders.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
+        self.listbox_folders = tk.Listbox(self.frame_tab_folders, width=50, height=20)
+        self.listbox_folders.pack(expand=True, fill=tk.BOTH)
+        self.listbox_folders.config(yscrollcommand=self.scrollbar_folders.set)
+        self.scrollbar_folders.config(command=self.listbox_folders.yview)
+
+        # Build Notebook
+        self.notebook_1.add(self.frame_tab_live, text='Live')
+        self.notebook_1.add(self.frame_tab_photos, text='Photos')
+        self.notebook_1.add(self.frame_tab_videos, text='Videos')
+        self.notebook_1.add(self.frame_tab_other, text='Other')
+        self.notebook_1.add(self.frame_tab_folders, text='Folders')
+        self.notebook_1.pack()
+
+        self.frame_list_1.grid(row=1, column=0, stick=tk.N + tk.E)
+        # END LEFT SIDE
+
+        # RIGHT SIDE
+        self.frame_list_2 = tk.Frame(self.frame)
+        # Buttons SubFrame
+        self.frame_buttons = tk.Frame(self.frame_list_2)
+        self.label_choose_dir = tk.Label(self.frame_buttons, text='1)')
+        self.button_choose_dir = tk.Button(self.frame_buttons, text='Select Folder',
+                                           command=lambda: self.populate_notebook(), width=20)
+        self.label_run = tk.Label(self.frame_buttons, text='2)')
+        self.button_run = tk.Button(self.frame_buttons, text='Run', command=self.run, width=20)
+        self.label_choose_dir.grid(row=0, column=0)
+        self.button_choose_dir.grid(row=0, column=1)
+        self.label_run.grid(row=1, column=0)
+        self.button_run.grid(row=1, column=1)
         self.button_run['state'] = tk.DISABLED
-        self.button_choose_dir = tk.Button(self.list_2_frame, text='Select Folder', command=lambda: self.populate_listbox(listbox=self.listbox_1), width=20)
-        self.button_choose_dir.pack(side=tk.BOTTOM)
-        self.scrollbar_2 = tk.Scrollbar(self.list_2_frame)
+        self.frame_buttons.pack(side=tk.BOTTOM, fill=tk.Y, expand=True)
+
+        self.label_2 = tk.Label(self.frame_list_2, text='Progress Details')
+        self.label_2.pack(side=tk.TOP, fill=tk.X)
+        self.scrollbar_2 = tk.Scrollbar(self.frame_list_2)
         self.scrollbar_2.pack(side=tk.RIGHT, fill=tk.Y, pady=(0, 10))
-        self.listbox_2 = tk.Listbox(self.list_2_frame, width=50, height=15)
+        self.listbox_2 = tk.Listbox(self.frame_list_2, width=53, height=15)
         self.listbox_2.pack(pady=(0, 10))
         self.listbox_2.config(yscrollcommand=self.scrollbar_2.set)
         self.scrollbar_2.config(command=self.listbox_2.yview)
-        self.list_2_frame.grid(row=1, column=1, stick=tk.N)
 
-
-
+        self.frame_list_2.grid(row=1, column=1, stick=tk.N + tk.W, padx=(20,0))
+        # END RIGHT SIDE
 
         # ******************** STATUS BAR ********************
-        self.status_bar = tk.Label(self.master, text='Idle', bd=1, relief=tk.SUNKEN, anchor=tk.W)
-        self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.frame_status = tk.Frame(self.master, bd=1, relief=tk.SUNKEN)
+        self.frame_status.pack(side=tk.BOTTOM, fill=tk.X)
+        self.status_bar = tk.Label(self.frame_status, text=self.IDLE, anchor=tk.W)
+        self.status_bar.pack(side=tk.LEFT, fill=tk.X)
+        self.progress_bar = ttk.Progressbar(self.frame_status, orient='horizontal', mode='indeterminate')
 
         # Center GUI on user's screen
         self.center(self.master)
+
+    def activate_progressbar(self, enable=True):
+        # TODO - Needs more advanced implementation
+        '''
+        if enable:
+            self.progress_bar.pack(side=tk.RIGHT, fill=tk.X, expand=True)
+            self.progress_bar.start(50)
+        else:
+            self.progress_bar.pack_forget()
+            self.progress_bar.stop()
+        '''
 
     def add_log(self, msg):
         self.listbox_2.insert(tk.END, str(msg))
@@ -101,24 +177,71 @@ class MetadataEditorGui:
         if self.current_dir:
             self.status_bar['text'] = 'RUNNING...'
             self.master.update_idletasks()
-            #time.sleep(2)
-            rbd.modify_jpeg_filename(self.current_dir, log=self.add_log, verbose=True)
-            self.status_bar['text'] = 'Idle'
+            self.activate_progressbar()
+            ambd.modify_media_filenames(self.current_dir, log=self.add_log, verbose=True)
+            self.activate_progressbar(False)
+            self.status_bar['text'] = self.IDLE
+            self.update_notebook()
+            self.add_log('Check "' + self.current_dir + '" for the modified files.')
+            self.status_bar['text'] = 'Check "' + self.current_dir + '" for the modified files.'
         else:
             self.listbox_2.insert(tk.END, 'You need to select a folder.')
             self.status_bar['text'] = 'YOU MUST SELECT A FOLDER FIRST!'
             self.master.update_idletasks()
 
-    def populate_listbox(self, event=None, listbox=None):
-        listbox.delete(0, tk.END)# clear the listbox
-        self.current_dir = tkFileDialog.askdirectory()
-        jpgs, other, subdirs = rbd.list_dir_jpeg(self.current_dir)
-        self.label_1['text'] = str(os.path.basename(self.current_dir))
-        for item in jpgs:
-            listbox.insert(tk.END, os.path.basename(item))
-        if len(jpgs) > 0:
-            self.button_run['state'] = tk.NORMAL
-        rbd.directory_status(self.current_dir, self.add_log)
+    def update_notebook(self):
+        if self.current_dir:
+            self.status_bar['text'] = 'Analyzing files in selected folder...'
+            self.activate_progressbar()
+            self.master.update_idletasks()
+            self.listbox_live.delete(0, tk.END)
+            self.listbox_photos.delete(0, tk.END)
+            self.listbox_videos.delete(0, tk.END)
+            self.listbox_other.delete(0, tk.END)
+            self.listbox_folders.delete(0, tk.END)
+
+            # Determine if any files in folder are valid media files
+            lives, jpegs, movs, others, subdirs = ambd.list_dir_files(self.current_dir)
+            valid_count = len(lives) + len(jpegs) + len(movs)
+            # Add valid files names to list of files to be edited
+            for item in lives:
+                self.listbox_live.insert(tk.END, os.path.basename(item))
+            for item in jpegs:
+                self.listbox_photos.insert(tk.END, os.path.basename(item))
+            for item in movs:
+                self.listbox_videos.insert(tk.END, os.path.basename(item))
+            for item in others:
+                self.listbox_other.insert(tk.END, os.path.basename(item))
+            for item in subdirs:
+                self.listbox_folders.insert(tk.END, os.path.basename(item))
+
+            self.status_bar['text'] = self.IDLE
+
+            return valid_count
+
+    def populate_notebook(self, event=None, directory=None):
+        # Select a folder with media files to rename
+        self.status_bar['text'] = 'Selecting folder...'
+        self.master.update_idletasks()
+        if directory:
+            self.current_dir = directory
+        else:
+            self.current_dir = tkFileDialog.askdirectory()
+
+        if self.current_dir:
+            valid_count = self.update_notebook()
+
+            self.status_bar['text'] = 'Checking if "Run" can be enabled...'
+
+            if valid_count > 0:
+                self.button_run['state'] = tk.NORMAL
+
+            # Add details to logger on valid media status and change Status Bar
+            ambd.directory_status(self.current_dir, self.add_log)
+            self.status_bar['text'] = self.IDLE
+            self.activate_progressbar(False)
+            self.master.update_idletasks()
+
 
     def select_dir(self, event=None):
         print tkFileDialog.askdirectory()
@@ -157,39 +280,3 @@ if __name__ == '__main__':
     app = MetadataEditorGui(root)
     root.mainloop()
 
-'''
-def center(win):
-    """
-    centers a tkinter window
-    :param win: the root or Toplevel window to center
-    """
-    win.update_idletasks()
-    width = win.winfo_width()
-    frm_width = win.winfo_rootx() - win.winfo_x()
-    win_width = width + 2 * frm_width
-    height = win.winfo_height()
-    titlebar_height = win.winfo_rooty() - win.winfo_y()
-    win_height = height + titlebar_height + frm_width
-    x = win.winfo_screenwidth() // 2 - win_width // 2
-    y = win.winfo_screenheight() // 2 - win_height // 2
-    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-    win.deiconify()
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = App(root)
-    root.attributes('-alpha', 0.0)
-    menubar = tk.Menu(root)
-    filemenu = tk.Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Exit", command=root.destroy)
-    menubar.add_cascade(label="File", menu=filemenu)
-    root.config(menu=menubar)
-    frm = tk.Frame(root, bd=4, relief='raised')
-    frm.grid(row=0,column=0)
-    lab = tk.Label(frm, text='Hello World!', bd=4, relief='sunken')
-    lab.grid(ipadx=4, padx=4, ipady=4, pady=4)
-    root.geometry('500x500')
-    center(root)
-    root.attributes('-alpha', 1.0)
-    root.mainloop()
-'''
